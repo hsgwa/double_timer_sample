e290b77815769cc2f82a5eb615e719734992b654
hsgwa <hasegawa@isp.co.jp>
2020-12-01 04:46:57 +0000

    add: cmake, pacage, main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,49 @@
+#include <memory>
+#include <chrono>
+#include "rclcpp/rclcpp.hpp"
+
+using namespace std::chrono_literals;
+
+class DoubleTimer : public rclcpp::Node {
+public: 
+	DoubleTimer(): 
+	Node("double_timer_node"), timer1_exec_once_(false), timer2_exec_once_(false)
+	{
+		timer1_ = create_wall_timer(1s, [this](){
+			RCLCPP_INFO(this->get_logger(), "timer1 callback");
+
+			timer1_exec_once_ = true;
+			if (timer1_exec_once_ && timer2_exec_once_) {
+				rclcpp::shutdown();
+			}
+			this->timer1_->cancel();
+		});
+		timer2_ = create_wall_timer(1s, [this](){
+			RCLCPP_INFO(this->get_logger(), "timer2 callback");
+			timer2_exec_once_ = true;
+			if (timer1_exec_once_ && timer2_exec_once_) {
+				rclcpp::shutdown();
+			}
+			this->timer1_->cancel();
+		});
+
+	}
+private:
+	rclcpp::TimerBase::SharedPtr timer1_;
+	rclcpp::TimerBase::SharedPtr timer2_;
+	bool timer1_exec_once_;
+	bool timer2_exec_once_;
+};
+
+int main(int argc, char *argv[]) {
+	rclcpp::init(argc, argv);
+
+	rclcpp::executors::SingleThreadedExecutor exec;
+	auto node = std::make_shared<DoubleTimer>();
+	exec.add_node(node);
+
+	exec.spin();
+
+	rclcpp::shutdown();
+	return 0;
+}
